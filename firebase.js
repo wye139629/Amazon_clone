09972273450bc50124631eb3d3fe1cbabbf2a8e7
will <wye139629@gmail.com>
2021-05-23 17:27:22 +0800

    set up google login with nextAuth

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCObA-LSGNiYX-s6ypWP0B9XFlBYMvn78g",
+  authDomain: "clone-8037b.firebaseapp.com",
+  projectId: "clone-8037b",
+  storageBucket: "clone-8037b.appspot.com",
+  messagingSenderId: "313639681211",
+  appId: "1:313639681211:web:fbcbce0fe5685627443d9e"
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,18 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from '@heroicons/react/outline'
+import { session, signIn, signOut, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 
 function Header() {
+  const [ session ] = useSession();
+  const router = useRouter()
   return (
     <header>
       <div className="flex items-center bg-amazon_blue px-1 py-3 flex-grow">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={ ()=> router.push('/') }
             src='https://links.papareact.com/f90'
             width={150}
             height={40}
@@ -27,8 +32,12 @@ function Header() {
 
         {/* right */}
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div className='link'>
-            <p>Hello William Liu</p>
+          <div onClick={!session? signIn : signOut} className='link'>
+            <p>
+              {
+                session? `Hello ${session.user.name}`:  'Sign In'
+              }
+            </p>
             <p className='font-extrabold md:text-sm'>Account & Lists</p>
           </div>
 
@@ -37,7 +46,7 @@ function Header() {
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
 
-          <div className='relative link flex items-center'>
+          <div className='relative link flex items-center' onClick={ ()=> router.push('/checkout') }>
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
             <ShoppingCartIcon className="h-10"/>
             <p className='hidden font-extrabold md:text-sm md:block mt-2'>Basket</p>
